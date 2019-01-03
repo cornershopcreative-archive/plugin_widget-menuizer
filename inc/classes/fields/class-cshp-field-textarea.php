d4277dfb644c6504dec1648a2019c2aed77dd456
Xavier Serrano <xavier@cornershopcreative.com>
2019-01-03 16:09:58 -0500

    changed - fixed fatal error

--- inc/classes/fields/class-cshp-field-textarea.php
+++ inc/classes/fields/class-cshp-field-textarea.php
@@ -8,21 +8,23 @@ if ( ! defined( 'ABSPATH' ) ) {
 	die( 'No script kiddies please!' );
 }
 
-/**
- * CSHP_Field_Textarea Class.
- */
-class CSHP_Field_Textarea extends CSHP_Field {
+if ( ! class_exists( 'CSHP_Field_Textarea' ) ) {
 	/**
-	 * Outputting our field.
+	 * CSHP_Field_Textarea Class.
 	 */
-	public function output() {
-		$allowed_html = wp_kses_allowed_html();
-		// set the value.
-		$value = ( ! empty( get_option( $this->args->id ) ) ) ? get_option( $this->args->id ) : $this->args->default_value;
-		// echo the textarea field.
-		echo '<textarea name="' . esc_attr( $this->args->id ) . '" id="' . esc_attr( $this->args->id ) . '" rows="' . esc_attr( $this->args->rows ) . '" class="large-text code" >' . esc_textarea( $value ) . '</textarea>';
-		// echo the description.
-		// phpcs:ignore WordPress.XSS.EscapeOutput.OutputNotEscaped
-		echo '<p>' . $this->args->description . '</p>';
+	class CSHP_Field_Textarea extends CSHP_Field {
+		/**
+		 * Outputting our field.
+		 */
+		public function output() {
+			$allowed_html = wp_kses_allowed_html();
+			// set the value.
+			$value = ( ! empty( get_option( $this->args->id ) ) ) ? get_option( $this->args->id ) : $this->args->default_value;
+			// echo the textarea field.
+			echo '<textarea name="' . esc_attr( $this->args->id ) . '" id="' . esc_attr( $this->args->id ) . '" rows="' . esc_attr( $this->args->rows ) . '" class="large-text code" >' . esc_textarea( $value ) . '</textarea>';
+			// echo the description.
+			// phpcs:ignore WordPress.XSS.EscapeOutput.OutputNotEscaped
+			echo '<p>' . $this->args->description . '</p>';
+		}
 	}
 }
